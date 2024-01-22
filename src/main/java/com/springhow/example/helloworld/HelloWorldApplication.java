8363e4fcc0899b294cdb1ecd6e28e3dd60565b8e
dhirendra <133599653+taankdhirendra@users.noreply.github.com>
2024-01-22 06:28:36 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello World Gourav sharma!";
+        return "Hello World dhirendra!";
     }
 
 }
